d2320425802e2e88e85c0b3e16fd358c154376d4
ugurkurekci <ugurkurekci98@gmail.com>
2021-06-16 23:07:22 +0300

    commit

--- hrms/src/main/java/hrms/hrms/entities/concretes/Cities.java
+++ hrms/src/main/java/hrms/hrms/entities/concretes/Cities.java
@@ -2,13 +2,8 @@ package hrms.hrms.entities.concretes;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.type.descriptor.sql.SmallIntTypeDescriptor;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
